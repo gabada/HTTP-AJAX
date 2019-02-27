3d989f79d74caf3cc38932d5f552082f1face0d0
Gill Abada <gill@callifi.com>
2019-02-27 09:37:25 -0500

    add form for new friend.

--- friends/src/index.js
+++ friends/src/index.js
@@ -9,21 +9,38 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            friendsData: []
+            friendsData: [],
+            friend: '',
+            age: '',
+            email: ''
         }
     }
 
     componentDidMount() {
         axios.get('http://localhost:5000/friends')
             .then(response => {
-                console.log(response);
-                this.setState({friendsData: response.data})
-            });
+                this.setState({friendsData: response.data});
+            }).catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    handleFriendChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
         return (
             <div className="App">
+                <form>
+                <input type="text" placeholder="new friend name" onChange={this.handleFriendChange} name="friend" value={this.state.friend} />
+                <input type="text" placeholder="new friend age" onChange={this.handleFriendChange} name="age" value={this.state.age} />
+                <input type="text" placeholder="new friend email" onChange={this.handleFriendChange} name="email" value={this.state.email} />
+                </form>
+                <button onClick={this.handleSubmitFriend}>Add New Friend</button>
                 <FriendsList friends={this.state.friendsData} />
             </div>
         )
