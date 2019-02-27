12eec327fbcb489e320b03911fa1727be23879ab
Gill Abada <gill@callifi.com>
2019-02-27 15:31:32 -0500

    add loading page when friendsData not available.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Friend from './Friend';
 
 const FriendsList = props => {
+    if (props.friends.length === 0) {
+        return <h1>Loading friends...</h1>
+    }
     return (
         <div>
             {props.friends.map(friend => {
-                return <Friend key={friend.id} friend={friend} handleDeleteFriend={props.handleDeleteFriend} />
+                return <Friend key={friend.id} friend={friend} handleDeleteFriend={props.handleDeleteFriend} handleUpdateFriend={props.handleUpdateFriend} />
             })}
         </div>
     );
